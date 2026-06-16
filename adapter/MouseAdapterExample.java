54b6a173b78ceb9dbf410ec2806e69df5a63888a
adhikari-anil <adhikarianil719@gmail.com>
2026-06-16 11:37:34 +0545

    mouseAdapter examples

--- adapter/MouseAdapterExample.java
+++ adapter/MouseAdapterExample.java
@@ -5,13 +5,35 @@ import java.awt.event.*;
 public class MouseAdapterExample extends MouseAdapter {
 
     Frame f;
+    int prevX, prevY;
 
     public MouseAdapterExample() {
         f = new Frame("Mouse Frame..");
         f.addMouseListener(this);
+        f.addMouseMotionListener(this);
         f.setLayout(null);
         f.setSize(400, 500);
         f.setVisible(true);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        prevX = e.getX();
+        prevY = e.getY();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        Graphics g = f.getGraphics();
+        g.setColor(Color.BLUE);
+
+        int x = e.getX();
+        int y = e.getY();
+
+        g.drawLine(prevX, prevY, x, y);
+
+        prevX = x;
+        prevY = y;
     }
 
     @Override
